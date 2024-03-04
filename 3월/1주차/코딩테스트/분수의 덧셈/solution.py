21629b6b64df183f41a1d71b15fd396b6b9387fe
changhyun-dev1 <145518676+changhyun-dev1@users.noreply.github.com>
2024-03-04 19:58:23 +0900

    Update solution.py

--- 3월/1주차/코딩테스트/분수의 덧셈/solution.py
+++ 3월/1주차/코딩테스트/분수의 덧셈/solution.py
@@ -1,1 +1,14 @@
+def solution(denum1, num1, denum2, num2):
+    answer = []
+    num = denum1*num2 + denum2*num1 # 분자
+    deno = num2*num1                # 분모
 
+    gcd = 0
+    # num과 deno의 최대공약수 구하기
+    for j in range(min(num,deno), 0, -1):
+        if num % j == 0 and deno % j ==0:
+            gcd = j
+            break
+
+    answer = [num//gcd, deno//gcd]
+    return answer
