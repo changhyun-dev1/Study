1f4712f818790333c259717fa21ba81f9d97134f
changhyun-dev1 <145518676+changhyun-dev1@users.noreply.github.com>
2024-04-06 05:46:51 +0900

    Update solution.py

--- 4월/코딩테스트/solution.py
+++ 4월/코딩테스트/solution.py
@@ -1,1 +1,37 @@
+# 의상
+def solution(clothes):
+    hash_map = {}
+    for clothe, type in clothes:
+        hash_map[type] = hash_map.get(type, 0) + 1
+        
+    answer = 1
+    for type in hash_map:   
+        answer *= (hash_map[type] + 1)
+    
+    return answer - 1
 
+print(solution([["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]))
+
+# 피로도
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = 0
+
+    all = list()
+    for i in permutations(dungeons, len(dungeons)):
+        all.append(i)
+    
+    count = list()
+    for i in all:
+        power = k
+        cnt = 0
+        for j in i:
+            if (j[0] <= power):
+                power -= j[1]
+                cnt += 1
+
+        count.append(cnt)
+
+    answer = max(count)
+    return answer
