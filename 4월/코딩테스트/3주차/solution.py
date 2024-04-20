02fc92f897a3ed7ae8fd0cbde79cc41a0f3c0e1f
changhyun-dev1 <145518676+changhyun-dev1@users.noreply.github.com>
2024-04-20 10:09:05 +0900

    Update solution.py

--- 4월/코딩테스트/3주차/solution.py
+++ 4월/코딩테스트/3주차/solution.py
@@ -1,1 +1,21 @@
+# 마법의 엘리베이터
 
+def solution(storey):
+    answer = 0
+
+    while storey:
+        remainder = storey % 10
+        if remainder > 5:
+            answer += (10 - remainder)
+            storey += 10
+        elif remainder < 5:
+            answer += remainder
+        else:
+            if (storey // 10) % 10 > 4:
+                storey += 10
+            answer += remainder
+        storey //= 10
+
+    return answer
+
+# 혼자서 하는 틱택토
