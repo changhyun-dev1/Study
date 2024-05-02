568e3e819affa3a52e5e59d21e45d74e12f50394
changhyun-dev1 <145518676+changhyun-dev1@users.noreply.github.com>
2024-05-03 00:50:44 +0900

    Update solution.py

--- 5월/코딩테스트/1주차/solution.py
+++ 5월/코딩테스트/1주차/solution.py
@@ -1,1 +1,13 @@
-
+# 완주하지 못한 선수
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    answer = ''
+  
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            return answer
+        answer = participant[-1]
+      
+    return answer
